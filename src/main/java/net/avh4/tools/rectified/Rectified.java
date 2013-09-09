c7043bffc3d5003f0d0b9114e6c926cbc303ce93
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-09-09 12:57:45 -0700

    Preview is locked to the correct size.

--- src/main/java/net/avh4/tools/rectified/Rectified.java
+++ src/main/java/net/avh4/tools/rectified/Rectified.java
@@ -19,11 +19,32 @@ public class Rectified {
         JFrame window = new JFrame("Rectified α");
         window.setLayout(new BorderLayout());
         final JComponent designPanel = new SwingSceneRenderer(app.designPanel()) {
+            @Override public Dimension getMinimumSize() {
+                return getPreferredSize();
+            }
+
             @Override public Dimension getPreferredSize() {
                 return new Dimension(320, 508);
             }
         };
-        window.add(designPanel, BorderLayout.WEST);
+        JPanel designWrapper = new JPanel(new GridBagLayout()) {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Graphics2D g2d = (Graphics2D) g;
+                g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+                int w = getWidth();
+                int h = getHeight();
+                Color color1 = new Color(0xffffff);
+                Color color2 = new Color(0xcccccc);
+                GradientPaint gp = new GradientPaint(0, 0, color1, w, h, color2);
+                g2d.setPaint(gp);
+                g2d.fillRect(0, 0, w, h);
+            }
+        };
+        designWrapper.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        designWrapper.add(designPanel);
+        window.add(designWrapper, BorderLayout.WEST);
         final JTextArea codePanel = new JTextArea() {
             @Override public Dimension getPreferredSize() {
                 return new Dimension(400, 400);
