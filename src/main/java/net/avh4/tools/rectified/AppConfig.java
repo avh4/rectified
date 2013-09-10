8b306057efb826488ac93fe6639cea84d6ea68e1
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-09-09 23:10:32 -0700

    Add more complete example

--- src/main/java/net/avh4/tools/rectified/AppConfig.java
+++ src/main/java/net/avh4/tools/rectified/AppConfig.java
@@ -1,11 +1,21 @@
 package net.avh4.tools.rectified;
 
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+
 public class AppConfig {
     public String appName() {
         return "Rectified α";
     }
 
     public String defaultDesign() {
-        return "{\"design\": [{\"color\":\"#eeeeee\"}]}";
+        try {
+            return IOUtils.toString(getClass().getResourceAsStream("example.json"));
+        } catch (IOException e) {
+            System.out.println("Failed to load example");
+            e.printStackTrace();
+            return "{\"design\": [{\"color\":\"#eeeeee\"}]}";
+        }
     }
 }
